02c20ee285ff6e526109ccfa67dfcccf908e971f
Jhonathan Alves <Jhonathanalves_br@hotmail.com>
2021-11-21 00:56:11 -0300

    update

--- src/main/java/one/digitalinnovation/personapi/Controlador.java
+++ src/main/java/one/digitalinnovation/personapi/Controlador.java
@@ -1,36 +1,60 @@
 package one.digitalinnovation.personapi;
 
+import one.digitalinnovation.personapi.Exception.PersonagemNaoExiste;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import one.digitalinnovation.personapi.*;
+
+import javax.servlet.annotation.HttpConstraint;
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/v1/pokemon")
+@RequestMapping("api/v1/pokemon")
 public class Controlador {
-
     @Autowired
     private Repositorio repositorio;
 
-    @PostMapping()
-    @ResponseBody()
-    public String addNewUser(@RequestParam String nome){
+    @PostMapping
+    @ResponseBody
+    public String adicionar(@Valid @RequestBody PokemonDTO pok) {
         Pokemon poke = new Pokemon();
-        poke.setNome(nome);
+        poke.setNome(pok.getNome());
         repositorio.save(poke);
-        return "Saved";
+        return "Criado!";
     }
 
-    /*@GetMapping()
-    @ResponseBody()
-    public String reposta(){
-        System.out.println("TESTE");
-        return "OK!";
-    }*/
-
-    @GetMapping()
-    public @ResponseBody
-    Iterable<Pokemon> getAllUsers() {
+    @GetMapping
+    @ResponseBody
+    public Iterable<Pokemon> getAllUsers() {
         return repositorio.findAll();
     }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Pokemon procruarPokemon(@PathVariable Integer id) throws PersonagemNaoExiste {
+        return procurarID(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public String excluir(@PathVariable Integer id) throws PersonagemNaoExiste {
+        procurarID(id);
+        repositorio.deleteById(id);
+        return "Deletado!";
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public String atualizar(@PathVariable Integer id, @RequestBody @Valid PokemonDTO pok) throws PersonagemNaoExiste {
+        Pokemon poke = procurarID(id);
+        poke.setNome(pok.getNome());
+        repositorio.save(poke);
+        return "Alterado!";
+    }
+
+    public Pokemon procurarID(Integer id) throws PersonagemNaoExiste {
+        return repositorio.findById(id).orElseThrow(() -> new PersonagemNaoExiste(id));
+    }
+
 }
